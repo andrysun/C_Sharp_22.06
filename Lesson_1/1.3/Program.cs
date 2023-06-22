1e9d3bfcddaffcc684cd04eb385a565ffe8e57c5
Andrew <andrej.salnikov.03@bk.ru>
2023-06-22 22:09:07 +0300

    Lesson_1 add new file 1.3

---
+++ Lesson_1/1.3/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = int.Parse(Console.ReadLine()!); //a = -6
+int count = -number; //count = 6
+    Console.Write(count + " ");
+while (count!=number){
+    if (number>0)
+    count++;//count=count+1
+    else
+    count--;//count=count-1
+    Console.Write(count + " ");
+}
