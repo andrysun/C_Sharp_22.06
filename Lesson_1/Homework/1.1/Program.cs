da2251c3073e3d7bef88080a3f197261b96f482b
Andrew <andrej.salnikov.03@bk.ru>
2023-06-25 17:31:57 +0300

    add new file with task 1.1 in file Homework

---
+++ Lesson_1/Homework/1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿// 1. Напишите программу, которая принимает на вход три числа
+//    и выдает максимальное из них
+
+Console.WriteLine("Введите три числа: ");
+int num_1 = int.Parse(Console.ReadLine()!);
+int num_2 = int.Parse(Console.ReadLine()!);
+int num_3 = int.Parse(Console.ReadLine()!);
+int max = 0;
+
+if (num_1 > max)
+{
+    max = num_1;
+}
+if (num_2 > max)
+{
+    max = num_2;
+}
+if (num_3 > max)
+{
+    max = num_3;
+}
+
+Console.WriteLine("Максимальное число: " + max);
