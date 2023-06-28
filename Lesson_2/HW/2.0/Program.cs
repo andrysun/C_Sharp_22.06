e727389f961f10f47e38848d49e6bb6405dcdab4
Andrew <andrej.salnikov.03@bk.ru>
2023-06-28 22:31:47 +0300

    feat: add new HW 2.0 - 2.3

---
+++ Lesson_2/HW/2.0/Program.cs
@@ -0,0 +1,13 @@
+﻿// 0. Напишите программу, которая принимает на вход трёхзначное число
+//    и на выходе показывает вторую цифру этого числа
+
+int Procedure(int num)
+{
+    int res_mid = num % 100;
+    int res = res_mid / 10;
+    return res;
+}
+
+int number = new Random().Next(100, 1000);
+int result = Procedure(number); 
+Console.WriteLine($"Вторая цифра числа {number} - {result}");
---
+++ Lesson_2/HW/2.1/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1. Напишите программу, которая выводит случайное трёхзначное число
+//    и удаляет вторую цифру этого числа
+
+void Div(int num_first, int num_second)
+{
+    Console.Write($"Получившееся число: {num_first}");
+    Console.Write(num_second);
+}
+
+int number = new Random().Next(100, 1000);
+int res_mid = number % 10;
+int res = number / 100;
+Div(res, res_mid);
+
+Console.Write(" Из числа: ");
+Console.Write(number);
