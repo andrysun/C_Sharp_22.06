d55e98b64dfdb0fff21d1a8c4a79a9ffcd160d7f
Andrew <andrej.salnikov.03@bk.ru>
2023-07-03 18:52:22 +0300

    Completing homework in files Lesson_3/HW/3.(0,1,2)

---
+++ Lesson_3/HW/3.2/Program.cs
@@ -0,0 +1,26 @@
+﻿// 2. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//    3 -> 1, 8, 27
+//    5 -> 1, 8, 27, 64, 125
+
+int number = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+if (number == 0) Console.WriteLine("1");
+
+if (number > 0)
+{
+    while (count <= number)
+    {
+        Console.Write(Math.Pow(count, 3) + " ");
+        count++;
+    }
+}
+else if (number < 0)
+{
+    count = 0;
+    while (number < count)
+    {
+        Console.Write(Math.Pow(number, 3) + " ");
+        number++;
+    }
+}
