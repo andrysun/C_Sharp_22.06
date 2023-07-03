f1178cf8eea5b87d959cd19d18fa1481f9f4d4c3
Andrew <andrej.salnikov.03@bk.ru>
2023-07-03 18:08:16 +0300

    feat: add new files with source code Lesson_3/3.(0,1,2,3)

---
+++ Lesson_3/3.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1. Напишите программу, которая по заданному номеру четверти,
+//    показывает диапазон возможных координат точек в этой четверти (x и y).
+
+void Quarters(int quater)
+{
+    if (quater > 4 || quater < 1)
+        Console.WriteLine("Error");
+    else if (quater == 1)
+        Console.WriteLine("x > 0 и y > 0");
+    else if (quater == 2)
+        Console.WriteLine("x < 0 и y > 0");
+    else if (quater == 3)
+        Console.WriteLine("x < 0 и y < 0");
+    else if (quater == 4)
+        Console.WriteLine("x > 0 и y < 0");
+}
+
+int num_quarter = int.Parse(Console.ReadLine()!);
+Quarters(num_quarter);
---
+++ Lesson_3/3.3/Program.cs
@@ -0,0 +1,16 @@
+﻿// 3. Напишите программу, которая принимает на вход число (N) 
+//    и выдаёт таблицу квадратов чисел от 1 до N.
+
+int number = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+if (number == 0)
+    Console.WriteLine("1");
+if (number > 0)
+{
+    while (count <= number)
+    {
+        Console.Write(Math.Pow(count, 2) + " ");
+        count++;
+    }
+}
