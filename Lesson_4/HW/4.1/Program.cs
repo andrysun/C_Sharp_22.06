7043d2796f6a801c259a26696ee4bf94ce7cca2d
Andrew <andrej.salnikov.03@bk.ru>
2023-07-09 15:52:12 +0300

    Completed task 4.1 in HW

---
+++ Lesson_4/HW/4.1/Program.cs
@@ -0,0 +1,27 @@
+﻿// 1. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//    452 -> 11
+//    82 -> 10
+//    9012 -> 12
+
+int Count(int num) // подсчёт цифр в числе
+{
+    int result = 0;
+    for (int i = 1; num > 0; i++)
+    {
+        num /= 10;
+        result = i;
+    }
+    return result;
+}
+
+int number = int.Parse(Console.ReadLine()!);
+int count_of_digit = Count(number);
+Console.WriteLine($"Количество разрядов: {count_of_digit}");
+
+int sum = 0;
+for (int i = 0; i < count_of_digit; i++)
+{
+    sum = sum + (number % 10);
+    number /= 10;
+}
+Console.WriteLine($"Сумма цифр в числе: {sum}");
