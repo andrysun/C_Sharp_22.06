87a276ce79456fa574a37e07ac3d2ba6302a81e8
Andrew <andrej.salnikov.03@bk.ru>
2023-07-17 19:04:40 +0300

    feat: add Homework on 6_lesson branch

---
+++ Lesson_7/7.0/Program.cs
@@ -0,0 +1,37 @@
+﻿// 0. Задача 1: Задайте двумерный массив размера m на n, каждый
+//    элемент в массиве находится по формуле: A[][] = m+n.
+//    Выведите полученный массив на экран.
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    Console.WriteLine("\nМассив:");
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = i+j;
+        }
+    }
+}
+
+
+
+Console.WriteLine("Введите количество столбцов ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество строк ");
+int n = int.Parse(Console.ReadLine()!);
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+PrintArray(matrix);
---
+++ Lesson_7/7.2/Program.cs
@@ -0,0 +1,39 @@
+﻿// 2. Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+//    https://4apple.org/summa-diagonalnyh-jelementov-matricy/
+//    https://ru.wikipedia.org/wiki/%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D0%B4%D0%B8%D0%B0%D0%B3%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C
+
+/*int DiagonalSum(int[,] arr)
+{
+    int res = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        res += arr[i, i];
+    }
+    return res;
+}
+
+void Sum_massive(int[,] array)
+{
+    int sum = 0;
+    if (array.GetLength(0) != array.GetLength(1))
+    {
+        Console.WriteLine("не будет ровной диагонали");
+        return;
+    }
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            if (i == j)
+            {
+                sum = sum + array[i, j];
+            }
+
+
+        }
+    }
+    Console.Write($"{sum}");
+
+}*/
