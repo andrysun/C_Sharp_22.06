2573f861a7afe5f2609c078d3c72201abee5c994
Andrew <andrej.salnikov.03@bk.ru>
2023-07-20 21:41:23 +0300

    completing classwork in file Lesson_9 in files 9.(0,1,2,3)

---
+++ Lesson_9/9.1/Program.cs
@@ -0,0 +1,10 @@
+﻿// 2. Задайте значения M и N. Напишите программу, которая 
+//    рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+void PrintNumber(int N, int M)
+{
+    if (N < M) return;
+    PrintNumber(N - 1, M);
+    Console.Write($"{N,2} ");
+}
+PrintNumber(9, 6);
---
+++ Lesson_9/9.3/Program.cs
@@ -0,0 +1,14 @@
+﻿// 4. Напишите программу, которая на вход принимает два числа A и B,
+//    и возводит число А в целую степень B с помощью рекурсии.
+
+Console.Write("Введите число основания степени: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите степень: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine(DegreeDigit(a, b));
+
+int DegreeDigit(int A, int B)
+{
+    if (B == 0) return 1;
+    return DegreeDigit(A, B - 1) * A;
+}
