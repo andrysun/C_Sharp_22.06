e889ae705b88d7cba5f6d29271fb10166b0302f2
Andrew <andrej.salnikov.03@bk.ru>
2023-07-22 21:48:20 +0300

    completing HW №9 in files Lesson_9/9.(0,1)

---
+++ Lesson_9/HW/9.0/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 0. Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные
+натуральные числа в промежутке от M до N с помощью рекурсии.
+
+M = 1; N = 5 -> "2, 4"
+M = 4; N = 8 -> "4, 6, 8" */
+
+void Print(int m, int n)
+{
+    if (m > n) return;
+    if (m % 2 == 0) Console.Write($"{m,3}");
+    Print(m + 1, n);
+}
+Console.Write("Введите начальное число: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конечное число: ");
+int N = int.Parse(Console.ReadLine()!);
+Print(M, N);
